45ba98f48d728166c48343df0b7da4541c35cc13
Arturo Cardenal <arturo.cardenal.ls@gmail.com>
2023-12-15 23:46:08 +0100

    pruebas

---
+++ parte-1/CSP-calls.sh
@@ -0,0 +1,7 @@
+# py CSPParking.py ./CSP-tests/parking00_enunciado.txt
+py CSPParking.py ./CSP-tests/parking01.txt
+py CSPParking.py ./CSP-tests/parking02.txt
+py CSPParking.py ./CSP-tests/parking03.txt
+py CSPParking.py ./CSP-tests/parking04.txt
+py CSPParking.py ./CSP-tests/parking05.txt
+py CSPParking.py ./CSP-tests/parking06.txt
