a74afac3934379824fb6fa93457942076988c4f1
Arturo Cardenal <arturo.cardenal.ls@gmail.com>
2023-12-15 13:40:48 +0100

    primera version restriccion maniobrabilidad

--- CSPParking.py
+++ CSPParking.py
@@ -19,12 +19,14 @@ for i in range(filas):
 print("Todas las plazas:", plazas_parking)
 
 # Plazas con electricidad del parking
-plazas_electricas = [(1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (3, 2)]
+# plazas_electricas = [(1, 1), (1, 2), (2, 1), (4, 1), (5,1), (5, 2)]
+plazas_electricas = [(1, 1), (1, 2), (2, 1), (2, 2), (3,1), (3, 2)]
 print("Plazas eléctricas:", plazas_electricas)
 
 
 # Creacion de las variables
-vehiculos = ('1-TSU-C', '2-TNU-X', '3-TNU-X', '4-TNU-C', '5-TSU-X', '6-TNU-X', '7-TNU-C', '8-TSU-C')
+# vehiculos = ('1-TSU-C', '2-TNU-X', '3-TNU-X', '4-TNU-C', '5-TSU-X', '6-TNU-X', '7-TNU-C', '8-TSU-C')
+vehiculos = ('1-TNU-X', '2-TNU-X', '3-TNU-X')
 
 def tiene_congelador(vehiculo):
     split_nombre = vehiculo.split("-")
@@ -40,16 +42,6 @@ for vehiculo in vehiculos:
     else:
         problem.addVariable(vehiculo, plazas_parking)
 
-"""
-problem.addVariable('1-TSU-C', plazas_electricas)
-problem.addVariable('2-TNU-X', plazas_parking)
-problem.addVariable('3-TNU-X', plazas_parking)
-problem.addVariable('4-TNU-C', plazas_electricas)
-problem.addVariable('5-TSU-X', plazas_parking)
-problem.addVariable('6-TNU-X', plazas_parking)
-problem.addVariable('7-TNU-C', plazas_electricas)
-problem.addVariable('8-TSU-C', plazas_electricas)
-"""
 
 
 # Creación de las restricciones
@@ -83,31 +75,59 @@ for vehiculo in vehiculos:
         for otro_vehiculo in vehiculos:
             if ((vehiculo != otro_vehiculo) and (es_TSU(otro_vehiculo) == False)):
                 problem.addConstraint(delante_de, (vehiculo, otro_vehiculo))
-                print(vehiculo, "delante_de", otro_vehiculo)
+                # print(vehiculo, "delante_de", otro_vehiculo)
 
-"""
-problem.addConstraint(delante_de, ('1-TSU-C', '2-TNU-X'))
-problem.addConstraint(delante_de, ('1-TSU-C', '3-TNU-X'))
-problem.addConstraint(delante_de, ('1-TSU-C', '4-TNU-C'))
-problem.addConstraint(delante_de, ('1-TSU-C', '6-TNU-X'))
-problem.addConstraint(delante_de, ('1-TSU-C', '7-TNU-C'))
-problem.addConstraint(delante_de, ('5-TSU-X', '2-TNU-X'))
-problem.addConstraint(delante_de, ('5-TSU-X', '3-TNU-X'))
-problem.addConstraint(delante_de, ('5-TSU-X', '4-TNU-C'))
-problem.addConstraint(delante_de, ('5-TSU-X', '6-TNU-X'))
-problem.addConstraint(delante_de, ('5-TSU-X', '7-TNU-C'))
-problem.addConstraint(delante_de, ('8-TSU-C', '2-TNU-X'))
-problem.addConstraint(delante_de, ('8-TSU-C', '3-TNU-X'))
-problem.addConstraint(delante_de, ('8-TSU-C', '4-TNU-C'))
-problem.addConstraint(delante_de, ('8-TSU-C', '6-TNU-X'))
-problem.addConstraint(delante_de, ('8-TSU-C', '7-TNU-C'))
-"""
 
 
 # 5. Por cuestiones de maniobrabilidad dentro del parking todo vehículo debe tener libre una plaza a la izquierda o derecha (mirando en dirección a la salida)
+def tiene_un_lado_libre(*vehiculos):
+    for vehiculo in vehiculos:
+        posicion_izquierda = (vehiculo[0]-1, vehiculo[1])
+        posicion_derecha = (vehiculo[0]+1, vehiculo[1])
+
+        derecha_libre = False
+        izquierda_libre = False
+
+        izquierda_disponible = True
+        derecha_disponible = True
+        if (posicion_izquierda not in plazas_parking):
+            izquierda_disponible = False
+        if (posicion_derecha not in plazas_parking):
+            derecha_disponible = False
+
+        for vehiculo_comparacion in vehiculos:
+            if vehiculo != vehiculo_comparacion:    # Comparar con los demas vehiculos, no consigo mismo
+                izquierda_ocupada = False
+                derecha_ocupada = False
+                izquierda_disponible = True
+                derecha_disponible = True
+                
+                if (vehiculo[1] == vehiculo_comparacion[1]):    # si estan en la misma columna
+                    if (posicion_izquierda[0] == vehiculo_comparacion[0]):
+                        izquierda_ocupada = True
+                    elif (posicion_derecha[0] == vehiculo_comparacion[0]):
+                        derecha_ocupada = True
+
+                if (derecha_disponible and derecha_ocupada == False):
+                    derecha_libre = True
+                if (izquierda_disponible and izquierda_ocupada == False):
+                    izquierda_libre = True
+                
+
+                print("---------------------------------------------------")
+                print("Comparar", vehiculo, "con", vehiculo_comparacion)
+                print("Derecha ocupada:", derecha_ocupada)
+                print("Derecha disponible:", derecha_disponible)
+                print("Derecha libre:", derecha_libre)
+                print("Izquierda ocupada:", izquierda_ocupada)
+                print("Izquierda disponible:", izquierda_disponible)
+                print("Izquierda libre:", izquierda_libre)
+
+        if (izquierda_libre or derecha_libre):
+            return True
 
 
-
+problem.addConstraint(tiene_un_lado_libre, vehiculos)
 
 
 
@@ -120,5 +140,10 @@ def imprimir_todas_soluciones(soluciones):
 	for solucion in soluciones:
 		print(solucion)
 
+def imprimir_soluciones(soluciones, numero_soluciones):
+	for i in range(numero_soluciones):
+		print(soluciones[i])
+
 print("Nº de soluciones:", len(todas_soluciones))
 # imprimir_todas_soluciones(todas_soluciones)
+imprimir_soluciones(todas_soluciones, 10)
--- pruebas.py
+++ pruebas.py
@@ -2,7 +2,7 @@ from constraint import *
 
 problem = Problem()
 
-plazas_electricas = [(1, 1), (1, 2), (2, 1), (2, 2)]
+plazas_electricas = [(1, 1), (1, 2), (1, 3), (2, 1), (2, 2), (2, 3), (3, 1), (3, 2), (3, 3)]
 
 # VARIABLES
 problem.addVariable('1-TSU-C', plazas_electricas)
@@ -11,25 +11,62 @@ problem.addVariable('7-TNU-C', plazas_electricas)
 problem.addVariable('8-TSU-C', plazas_electricas)
 
 
-
+vehiculos = ('1-TSU-C', '4-TNU-C', '7-TNU-C', '8-TSU-C')
 
 # RESTRICCIONES
 # 1. Todas diferentes
-problem.addConstraint(AllDifferentConstraint(), ('1-TSU-C', '4-TNU-C', '7-TNU-C', '8-TSU-C'))
+problem.addConstraint(AllDifferentConstraint(), vehiculos)
+
+
+
+# 3. Que tenga un lado libre (maniobrabilidad)
+def tiene_un_lado_libre(vehiculo):
+    izquierda_libre = False
+    derecha_libre = False
+    print("Vehiculo:" , vehiculo)
+    print("Izquierda Libre:" , izquierda_libre)
+    print("Derecha Libre:" , derecha_libre)
+    # for i in range (len(vehiculos)):
+        # print(vehiculo, vehiculo[0], vehiculo[1])
+        # print(vehiculos[i], vehiculos[i][0], vehiculos[i][1])
+        # print("------------")
+        # if ((vehiculo[1] == vehiculos[i][1]) and ((vehiculo[0])-1 == vehiculo[i][0])):
+        #     izquierda_libre = False
+        # else:
+        #     izquierda_libre = True
+        
+        # if ((vehiculo[1] == vehiculos[i][1]) and ((vehiculo[0])+1 == vehiculo[i][0])):
+        #     derecha_libre = False
+        # else:
+        #     derecha_libre = True
+
+    if (izquierda_libre or derecha_libre):
+        return True
+
+
+# tiene_un_lado_libre('1-TSU-C')
+
+
+problem.addConstraint(tiene_un_lado_libre, ('1-TSU-C', '4-TNU-C'))
+# problem.addConstraint(tiene_un_lado_libre, ('4-TNU-C'))
+# problem.addConstraint(tiene_un_lado_libre, ('7-TNU-C'))
+# problem.addConstraint(tiene_un_lado_libre, ('8-TSU-C'))
+
+
 
 
 # 2. Una delante de otra
-def delante_de(a, b):
-    if (a[0] == b[0]):
-        if (a[1] > b[1]):
-            return True
-    else:
-        return True
+# def delante_de(a, b):
+#     if (a[0] == b[0]):
+#         if (a[1] > b[1]):
+#             return True
+#     else:
+#         return True
 
-problem.addConstraint(delante_de, ('1-TSU-C', '4-TNU-C'))
-problem.addConstraint(delante_de, ('1-TSU-C', '7-TNU-C'))
-problem.addConstraint(delante_de, ('8-TSU-C', '4-TNU-C'))
-problem.addConstraint(delante_de, ('8-TSU-C', '7-TNU-C'))
+# problem.addConstraint(delante_de, ('1-TSU-C', '4-TNU-C'))
+# problem.addConstraint(delante_de, ('1-TSU-C', '7-TNU-C'))
+# problem.addConstraint(delante_de, ('8-TSU-C', '4-TNU-C'))
+# problem.addConstraint(delante_de, ('8-TSU-C', '7-TNU-C'))
 
 # def delante_de(a, b):
 #     if (a[1] > b[1]):
@@ -47,8 +84,8 @@ problem.addConstraint(delante_de, ('8-TSU-C', '7-TNU-C'))
 solutions1 = problem.getSolutions()
 
 print("Numero de soluciones:", len(solutions1))
-for solution in solutions1:
-    print(solution)
+# for solution in solutions1:
+    # print(solution)
 
 
 
